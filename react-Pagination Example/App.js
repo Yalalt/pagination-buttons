52165244da2ee75414a859fb6ee0d4a4381bbe0b
Yalalt <yalaltgandush@gmail.com>
2023-02-03 12:10:58 +0800

    Page pagination

---
+++ react-Pagination Example/App.js
@@ -0,0 +1,56 @@
+
+import { useMemo } from 'react';
+import './App.css';
+import Pagination from './Pagination';
+
+let PageSize = 10;
+
+function App() {
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const currentTableData = useMemomo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
+  return (
+    <div className="App">
+      <h2>Pagination text here</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>FIRST NAME</th>
+            <th>LAST NAME</th>
+            <th>EMAIL</th>
+            <th>PHONE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentTableData.map(item => {
+            return (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={data.length}
+        pageSize={PageSize}
+        onPageChange={page => setCurrentPage(page)}
+      />
+    </div>
+  );
+}
+
+export default App;
